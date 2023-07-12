a2fe7a9ec88464a1cc24a34da7eedf28d08566a1
Yuhong Sun <yuhongsun96@gmail.com>
2023-07-11 21:51:08 -0700

    not done yet

--- backend/danswer/connectors/bookstack/connector.py
+++ backend/danswer/connectors/bookstack/connector.py
@@ -8,20 +8,14 @@ from bs4 import BeautifulSoup
 from danswer.configs.app_configs import INDEX_BATCH_SIZE
 from danswer.configs.constants import DocumentSource
 from danswer.configs.constants import HTML_SEPARATOR
+from danswer.connectors.bookstack.client import BookStackApiClient
 from danswer.connectors.interfaces import GenerateDocumentsOutput
 from danswer.connectors.interfaces import LoadConnector
 from danswer.connectors.interfaces import PollConnector
 from danswer.connectors.interfaces import SecondsSinceUnixEpoch
-from danswer.connectors.bookstack.client import BookStackApiClient
+from danswer.connectors.models import ConnectorMissingCredentialError
 from danswer.connectors.models import Document
 from danswer.connectors.models import Section
-
-
-class BookstackClientNotSetUpError(PermissionError):
-    def __init__(self) -> None:
-        super().__init__(
-            "BookStack Client is not set up, was load_credentials called?"
-        )
 
 
 class BookstackConnector(LoadConnector, PollConnector):
@@ -53,14 +47,18 @@ class BookstackConnector(LoadConnector, PollConnector):
         params = {
             "count": str(self.batch_size),
             "offset": str(start_ind),
-            "sort": "+id"
+            "sort": "+id",
         }
 
         if start:
-            params["filter[updated_at:gte]"] = datetime.utcfromtimestamp(start).strftime('%Y-%m-%d %H:%M:%S')
+            params["filter[updated_at:gte]"] = datetime.utcfromtimestamp(
+                start
+            ).strftime("%Y-%m-%d %H:%M:%S")
 
         if end:
-            params["filter[updated_at:lte]"] = datetime.utcfromtimestamp(end).strftime('%Y-%m-%d %H:%M:%S')
+            params["filter[updated_at:lte]"] = datetime.utcfromtimestamp(end).strftime(
+                "%Y-%m-%d %H:%M:%S"
+            )
 
         batch = self.bookstack_client.get(endpoint, params=params).get("data", [])
         for item in batch:
@@ -76,24 +74,20 @@ class BookstackConnector(LoadConnector, PollConnector):
             sections=[Section(link=url, text=text)],
             source=DocumentSource.BOOKSTACK,
             semantic_identifier="Book: " + book.get("name"),
-            metadata={
-                "type": "book",
-                "updated_at": book.get("updated_at")
-            },
+            metadata={"type": "book", "updated_at": book.get("updated_at")},
         )
 
     def _chapter_to_document(self, chapter: dict):
-        url = self.bookstack_client.build_app_url("/books/" + chapter.get("book_slug") + "/chapter/" + chapter.get("slug"))
+        url = self.bookstack_client.build_app_url(
+            "/books/" + chapter.get("book_slug") + "/chapter/" + chapter.get("slug")
+        )
         text = chapter.get("name", "") + "\n" + chapter.get("description", "")
         return Document(
             id="chapter:" + str(chapter.get("id")),
             sections=[Section(link=url, text=text)],
             source=DocumentSource.BOOKSTACK,
             semantic_identifier="Chapter: " + chapter.get("name"),
-            metadata={
-                "type": "chapter",
-                "updated_at": chapter.get("updated_at")
-            },
+            metadata={"type": "chapter", "updated_at": chapter.get("updated_at")},
         )
 
     def _shelf_to_document(self, shelf: dict):
@@ -104,17 +98,21 @@ class BookstackConnector(LoadConnector, PollConnector):
             sections=[Section(link=url, text=text)],
             source=DocumentSource.BOOKSTACK,
             semantic_identifier="Shelf: " + shelf.get("name"),
-            metadata={
-                "type": "shelf",
-                "updated_at": shelf.get("updated_at")
-            },
+            metadata={"type": "shelf", "updated_at": shelf.get("updated_at")},
         )
 
     def _page_to_document(self, page: dict):
         page_id = str(page.get("id"))
         page_data = self.bookstack_client.get("/pages/" + page_id, {})
-        url = self.bookstack_client.build_app_url("/books/" + page.get("book_slug") + "/page/" + page_data.get("slug"))
-        page_html = "<h1>" + html.escape(page_data.get("name")) + "</h1>" + page_data.get("html")
+        url = self.bookstack_client.build_app_url(
+            "/books/" + page.get("book_slug") + "/page/" + page_data.get("slug")
+        )
+        page_html = (
+            "<h1>"
+            + html.escape(page_data.get("name"))
+            + "</h1>"
+            + page_data.get("html")
+        )
         soup = BeautifulSoup(page_html, "html.parser")
         text = soup.get_text(HTML_SEPARATOR)
         time.sleep(0.1)
@@ -123,15 +121,12 @@ class BookstackConnector(LoadConnector, PollConnector):
             sections=[Section(link=url, text=text)],
             source=DocumentSource.BOOKSTACK,
             semantic_identifier="Page: " + page_data.get("name"),
-            metadata={
-                "type": "page",
-                "updated_at": page_data.get("updated_at")
-            },
+            metadata={"type": "page", "updated_at": page_data.get("updated_at")},
         )
 
     def load_from_state(self) -> GenerateDocumentsOutput:
         if self.bookstack_client is None:
-            raise BookstackClientNotSetUpError()
+            raise ConnectorMissingCredentialError("Bookstack")
 
         return self.poll_source(None, None)
 
@@ -139,7 +134,7 @@ class BookstackConnector(LoadConnector, PollConnector):
         self, start: SecondsSinceUnixEpoch | None, end: SecondsSinceUnixEpoch | None
     ) -> GenerateDocumentsOutput:
         if self.bookstack_client is None:
-            raise BookstackClientNotSetUpError()
+            raise ConnectorMissingCredentialError("Bookstack")
 
         transform_by_endpoint: dict[str, Callable[[dict], Document]] = {
             "/books": self._book_to_document,
@@ -151,7 +146,9 @@ class BookstackConnector(LoadConnector, PollConnector):
         for endpoint, transform in transform_by_endpoint.items():
             start_ind = 0
             while True:
-                doc_batch, num_results = self._get_doc_batch(endpoint, transform, start_ind, start, end)
+                doc_batch, num_results = self._get_doc_batch(
+                    endpoint, transform, start_ind, start, end
+                )
                 start_ind += num_results
                 if doc_batch:
                     yield doc_batch
